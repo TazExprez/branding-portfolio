068c10523b10f3e0bfbc263e01ae60bcfdd68571
Luis Rodriguez <luisrod22@gmail.com>
2020-07-26 05:13:29 -0400

    Finished styling the main index.html page.  Added a menu that slides from the left side of the page with the press of a hamburger button.  Added JavaScript to control the menu.

--- gulpfile.js
+++ gulpfile.js
@@ -14,8 +14,7 @@ const watch = () => {
 
   gulp.watch("./css/*.css", injectCss);
   gulp.watch("./*.html").on("change", browserSync.reload);
-  // Commented this out in case Joe does not use JavaScript.  If he does, then look at how Joe does his folders and make any changes to the code below.  ./ means the root folder, basically the branding-portfolio folder.  js/ means the JavaScript folder.  Joe may call it this, or he may not.  He might not even put it inside a folder.  If he doesn't, then go by the gulp.watch just above this one for more guidance.  ** means any folder inside of this folder.  *.js means any JavaScript file.  The rest of the code from .on onwards just says to reload the browser when any JavaScript file changes.
-  //   gulp.watch("./js/**/*.js").on("change", browserSync.reload);
+  // gulp.watch("./js/*.js").on("change", browserSync.reload);
 };
 
 exports.default = watch;
